14205f9a3b28930de61fe66f62b3536af70ba7e6
Oleg Sucharevich <olegs@codefresh.io>
2019-01-03 20:35:42 +0200

    implement codefresh API to validate and sign runtime environment

--- pkg/codefresh/runtime_enrionment.go
+++ pkg/codefresh/runtime_enrionment.go
@@ -6,6 +6,8 @@ type (
 	// IRuntimeEnvironmentAPI declers Codefresh runtime environment API
 	IRuntimeEnvironmentAPI interface {
 		CreateRuntimeEnvironment(*CreateRuntimeOptions) (*RuntimeEnvironment, error)
+		ValidateRuntimeEnvironment(*ValidateRuntimeOptions) error
+		SignRuntimeEnvironmentCertificate(*SignCertificatesOptions) ([]byte, error)
 	}
 
 	RuntimeEnvironment struct {
@@ -18,6 +20,16 @@ type (
 		HasAgent  bool
 	}
 
+	ValidateRuntimeOptions struct {
+		Cluster   string
+		Namespace string
+	}
+
+	SignCertificatesOptions struct {
+		AltName string
+		CSR     string
+	}
+
 	createRuntimeEnvironmentResponse struct {
 		Name string
 	}
@@ -25,7 +37,6 @@ type (
 
 // CreateRuntimeEnvironment - create Runtime-Environment
 func (c *codefresh) CreateRuntimeEnvironment(opt *CreateRuntimeOptions) (*RuntimeEnvironment, error) {
-	// r := &createRuntimeEnvironmentResponse{}
 	re := &RuntimeEnvironment{
 		Name: fmt.Sprintf("%s/%s", opt.Cluster, opt.Namespace),
 	}
@@ -48,7 +59,35 @@ func (c *codefresh) CreateRuntimeEnvironment(opt *CreateRuntimeOptions) (*Runtim
 
 	if resp.StatusCode < 400 {
 		return re, nil
-	} else {
-		return nil, fmt.Errorf("Error during runtime environment creation")
 	}
+	return nil, fmt.Errorf("Error during runtime environment creation")
+}
+
+func (c *codefresh) ValidateRuntimeEnvironment(opt *ValidateRuntimeOptions) error {
+	body := map[string]interface{}{
+		"clusterName": opt.Cluster,
+		"namespace":   opt.Namespace,
+	}
+	_, err := c.requestAPI(&requestOptions{
+		path:   "/api/custom_clusters/validate",
+		method: "POST",
+		body:   body,
+	})
+	return err
+}
+
+func (c *codefresh) SignRuntimeEnvironmentCertificate(opt *SignCertificatesOptions) ([]byte, error) {
+	body := map[string]interface{}{
+		"reqSubjectAltName": opt.AltName,
+		"csr":               opt.CSR,
+	}
+	resp, err := c.requestAPI(&requestOptions{
+		path:   "/api/custom_clusters/signServerCerts",
+		method: "POST",
+		body:   body,
+	})
+	if err != nil {
+		return nil, err
+	}
+	return resp.Bytes(), nil
 }
