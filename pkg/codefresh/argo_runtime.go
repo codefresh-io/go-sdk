bf4bfc6297acb953dafaf581a14a3b1d9b2109f6
Oren Gurfinkel <oren.gurfinkel@codefresh.io>
2021-07-05 15:33:07 +0300

    wip

--- pkg/codefresh/argo_runtime.go
+++ pkg/codefresh/argo_runtime.go
@@ -8,7 +8,6 @@ import (
 	"github.com/codefresh-io/argo-platform/libs/ql/graph/model"
 )
 
-
 type (
 	IArgoRuntimeAPI interface {
 		List() ([]model.Runtime, error)
@@ -17,20 +16,20 @@ type (
 		codefresh *codefresh
 	}
 	graphqlRuntimesResponse struct {
-		Data  struct {
-			Runtimes model.RuntimePage `json:"runtimes"`
-		} `json:"data"`
-		Errors []byte
+		Data struct {
+			Runtimes model.RuntimePage
+		}
+		Errors []graphqlError
 	}
 )
 
 func newArgoRuntimeAPI(codefresh *codefresh) IArgoRuntimeAPI {
 	return &argoRuntime{codefresh: codefresh}
 }
-func (r *argoRuntime)  List() ([]model.Runtime, error) {
+func (r *argoRuntime) List() ([]model.Runtime, error) {
 
 	jsonData := map[string]interface{}{
-        "query":` 
+		"query": ` 
 		{
 			runtimes{
 			  edges{
@@ -46,24 +45,23 @@ func (r *argoRuntime)  List() ([]model.Runtime, error) {
 			}
 		  }
         `,
-    }
-
+	}
 
 	response, err := r.codefresh.requestAPI(&requestOptions{
 		method: "POST",
-		path: "/argo/api/graphql",
-		body:  jsonData,
+		path:   "/argo/api/graphql",
+		body:   jsonData,
 	})
-    defer response.Body.Close()
-    if err != nil {
-        fmt.Printf("The HTTP request failed with error %s\n", err)
-		return nil, err
-    }
-    data, err := ioutil.ReadAll(response.Body)
+	defer response.Body.Close()
 	if err != nil {
-        fmt.Printf("failed to read from response body")
+		fmt.Printf("The HTTP request failed with error %s\n", err)
 		return nil, err
-    }
+	}
+	data, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		fmt.Printf("failed to read from response body")
+		return nil, err
+	}
 	res := graphqlRuntimesResponse{}
 	err = json.Unmarshal(data, &res)
 	if err != nil {
@@ -73,8 +71,11 @@ func (r *argoRuntime)  List() ([]model.Runtime, error) {
 	for _, v := range res.Data.Runtimes.Edges {
 		runtimes = append(runtimes, *v.Node)
 	}
-	
+
+	if len(res.Errors) > 0 {
+		return nil, graphqlErrorResponse{errors: res.Errors}
+	}
+
 	return runtimes, nil
-	
-	
+
 }
