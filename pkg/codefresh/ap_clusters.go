128e84a6ae3025fc8ba8064f13137d2dd897117d
Noam Gal <noam.gal@codefresh.io>
2022-04-17 21:13:21 +0300

    added AddRollouts func

--- pkg/codefresh/ap_clusters.go
+++ pkg/codefresh/ap_clusters.go
@@ -3,19 +3,18 @@ package codefresh
 import (
 	"context"
 	"fmt"
-
-	// model "github.com/codefresh-io/go-sdk/pkg/codefresh/model/app-proxy"
 )
 
 type (
 	IAppProxyClustersAPI interface {
+		AddRollouts(ctx context.Context, server string, namespace string) error
 		RemoveCluster(ctx context.Context, server string, runtime string) error
 	}
 	appProxyClusters struct {
 		codefresh *codefresh
 	}
 
-	graphqlClusterRemoveResponse struct {
+	graphqlClusterResponse struct {
 		Errors []graphqlError
 	}
 )
@@ -24,7 +23,34 @@ func newAppProxyClustersAPI(c *codefresh) IAppProxyClustersAPI {
 	return &appProxyClusters{codefresh: c}
 }
 
-func (c *appProxyClusters) RemoveCluster(ctx context.Context, server string, runtime string) error {
+func (c *appProxyClusters)AddRollouts(ctx context.Context, server string, namespace string) error {
+	jsonData := map[string]interface{}{
+		"query": `
+			mutation createRollouts($args: CreateRolloutsInput!) {
+				createRollouts(args: $args)
+			}
+		`,
+		"variables": map[string]interface{}{
+			"destServer": server,
+			"destNamespace": namespace,
+		},
+	}
+
+	res := &graphqlClusterResponse{}
+	err := c.codefresh.graphqlAPI(ctx, jsonData, res)
+
+	if err != nil {
+		return fmt.Errorf("failed making a graphql API call to add rollouts: %w", err)
+	}
+
+	if len(res.Errors) > 0 {
+		return graphqlErrorResponse{errors: res.Errors}
+	}
+
+	return nil
+}
+
+func (c *appProxyClusters)RemoveCluster(ctx context.Context, server string, runtime string) error {
 	jsonData := map[string]interface{}{
 		"query": `
 			mutation RemoveCluster($server: String!, $runtime: String!) {
@@ -37,7 +63,7 @@ func (c *appProxyClusters) RemoveCluster(ctx context.Context, server string, run
 		},
 	}
 
-	res := &graphqlClusterRemoveResponse{}
+	res := &graphqlClusterResponse{}
 	err := c.codefresh.graphqlAPI(ctx, jsonData, res)
 
 	if err != nil {
